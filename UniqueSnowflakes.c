e7e580191ed20328d223ec9ac24694f48c8c75e9
Cash Myers <cmyers880@gmail.com>
2022-10-18 11:47:20 -0500

    fix: corrected two operations errors: 1) Reorder functions to avoid implicit call error 2) Changed output text to match requirements for online Judge.

--- UniqueSnowflakes.c
+++ UniqueSnowflakes.c
@@ -1,30 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define SIZE 100000
 
 // NOTE: This approach follows a design pattern in which the comparative array is transversed in both directions.
 //       Binary Search Trees are an example of a "Tranvserse in Both Directions" design pattern.
-
-
-// initial function to determine concept of unique snowflakes
-// using a simple array
-// void find_identical(int values[], int n) {
-//     int i, j;
-//     for (i = 0; i < n; i++) {
-
-         // Start at the next element, otherwise we are comparing the same element
-         // and will get a false positive.
-//         for (j = i + 1; j < n; j++) {
-//             if (values[i] == values[j]) {
-//                 printf("Twin integers found.\n");
-//                 return;
-//             }
-//         }
-
-//     }
-//     printf("No two integers are the identical.\n");
-// }
-
 
 // Transverse "RIGHT" to determine match.
 // If match is found, return 1, otherwise return 0. (Boolean)
@@ -67,15 +47,33 @@ int compare_flakes(int flake1[], int flake2[]) {
     return 0;
 }
 
+// i\Initial function to determine concept of unique snowflakes
+void find_identical(int flakes[][6], int n) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+
+        // Start at the next element, otherwise we are comparing the same element
+        // and will get a false positive.
+        for (j = i + 1; j < n; j++) {
+            if (compare_flakes(flakes[i], flakes[j])) {
+                printf("Twin snowflakes found.\n");
+                return;
+            }
+        }
+    }
+    printf("No two snowflakes are alike.\n");
+}
+
 // As the std sizeOf operation on an array is not supported in C, and must be calculated
 // manually, we will use a hard-coded value for now (in order to optimize processing time).
 int main(void) {
-    int a[6] = {1, 2, 3, 4, 5, 6};
-    int b[6] = {3, 2, 1, 6, 5, 4};
-    if (compare_flakes(a, b) == 1) {
-        printf("The flakes are identical.\n");
-    } else {
-        printf("The flakes are not identical.\n");
-    }
+    // static is used to ensure that the array is allocated its own memory space, due to its size.
+    static int flakes[SIZE][6];
+    int n, i, j;
+    scanf("%d", &n);
+    for(i=0; i <n; i++)
+        for (j=0; j<6; j++)
+            scanf("%d", &flakes[i][j]);
+    find_identical(flakes, n);
     return 0;
 }
